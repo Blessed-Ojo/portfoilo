9b36be5ed08d7efa34c22de6bfa92b20dc299aab
Blessed-Ojo <ojoblessed533@gmail.com>
2025-08-10 14:32:45 +0100

    Add MainLayout and MainContent components for improved layout structure

---
+++ code-ninja/src/components/main-content.tsx
@@ -0,0 +1,17 @@
+import Hero from "@/components/hero";
+import ProjectGrid from "@/components/project-grid"
+import WorkHistory from "@/components/work-history";
+import SkillsSection from "@/components/skills-section";
+
+
+export default function MainContent() {
+  return(
+    <main>
+      <Hero/>
+       <ProjectGrid/>
+       <WorkHistory/>
+       <SkillsSection/>
+
+    </main>
+  )
+}
